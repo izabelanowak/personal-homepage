8be8a7293c2bdd25f34f9ebbf6744e0a715844e8
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-29 14:37:14 +0200

    Section component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { skillset, whatToLearnNext } from "./lists";
+import Section from "./Section";
+
 function App() {
   return (
     <>
+      <Section
+        title="My skillset includes"
+        list={skillset}
+      />
+      <Section
+        title="What I want to learn next"
+        list={whatToLearnNext}
+      />
     </>
   );
 }
---
+++ src/Section/index.js
@@ -0,0 +1,19 @@
+import { Content, Item, List, StyledSection, Title } from "./styled";
+
+const Section = ({ title, list }) => (
+    <StyledSection>
+        <Title>{title}
+        </Title>
+        <List>
+            {list.map(item => (
+                <Item key={item}>
+                    <Content>
+                        {item}
+                    </Content>
+                </Item>
+            ))}
+        </List>
+    </StyledSection>
+);
+
+export default Section;
