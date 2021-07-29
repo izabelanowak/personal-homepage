f38571897c6a1845f04ba145db70696efaec3cf7
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-29 14:36:42 +0200

    global style and theme added

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+    colors: {
+        lightMode: {
+            textPrimary: "#252525",
+            textSecondary: "#6E7E91",
+            white: "#FFFFFF",
+            mainAccent: "#0366D6",
+            lightGray: "#E5E5E5",
+        },
+        darkMode: {
+            textPrimary: "#FFFFFF",
+            textSecondary: "#FFFFFF",
+            white: "#FFFFFF",
+            mainAccent: "#2188FF",
+        }
+    },
+
+    breakpoints: {
+        mobile: 767,
+    }
+};
