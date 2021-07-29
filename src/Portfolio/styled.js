27200a604130f0c532c4ccc87b7093e03d9caca1
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-29 23:06:52 +0200

    Portfolio style added

---
+++ src/Portfolio/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Logo = styled.img`
+
+`;
+
+export const Title = styled.h2`
+    font-weight: 900;
+    font-size: 30px;
+    line-height: 36px;
+    text-align: center;
+`;
+
+export const Paragraph = styled.p`
+    font-size: 20px;
+    line-height: 28px;
+    text-align: center;
+`;
+
+export const Body = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`;
