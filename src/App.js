cfe63bb48961bb352794715f5beed52c11653977
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-30 20:35:57 +0200

    header and Portfolio added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Button } from "./Button";
 import { skillset, whatToLearnNext } from "./lists";
+import Module from "./Module";
+import Portfolio from "./Portfolio";
 import Section from "./Section";
+import messageLogo from './message.svg';
 
 function App() {
   return (
     <>
+      <header>
+        <Module
+          text="This is"
+          title="Izabela Nowak"
+          description="Hi! I'm future Frontend Developer with passion about CSS, React and in love with Styled-components"
+          extraContent={
+            <Button><img src={messageLogo} alt="Message Logo" /> Hire me</Button>
+          }
+        />
+      </header>
       <Section
         title="My skillset includes"
         list={skillset}
@@ -12,8 +26,9 @@ function App() {
         title="What I want to learn next"
         list={whatToLearnNext}
       />
+      <Portfolio />
     </>
   );
-}
+};
 
 export default App;
