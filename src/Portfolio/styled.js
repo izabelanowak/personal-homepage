03554950d2969a8c5b2321b1bf90add7fa1c3578
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-30 13:23:24 +0200

    Portfolio style added(text-align, margin,grid-gap)

--- src/Portfolio/styled.js
+++ src/Portfolio/styled.js
@@ -1,23 +1,29 @@
 import styled from "styled-components";
 
-export const Logo = styled.img`
+export const Wrapper = styled.div`
+    text-align: center;
+`;
 
+export const Logo = styled.svg`
+    margin: 0 0 13px 0;
 `;
 
 export const Title = styled.h2`
     font-weight: 900;
     font-size: 30px;
     line-height: 36px;
-    text-align: center;
+    margin: 0 0 8px 0;
 `;
 
 export const Paragraph = styled.p`
     font-size: 20px;
     line-height: 28px;
-    text-align: center;
+    margin: 0 0 24px 0;
 `;
 
 export const Body = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
+    grid-gap: 32px;
+    text-align: left;
 `;
