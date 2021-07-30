cf1cf699796b6802c41f56221466746ec794edc3
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-30 13:20:48 +0200

    unnecessary styles removed (margin, font-size)

--- src/Portfolio/Teil/styled.js
+++ src/Portfolio/Teil/styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    margin: 10px;
     background-color: ${({ theme }) => theme.colors.lightMode.white};
     border: 6px solid rgba(209, 213, 218, 0.3);
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
@@ -25,15 +24,13 @@ export const Header = styled.h3`
 
 export const Description = styled.p`
     margin: 24px 0;
-    font-size: 18px;
     line-height: 25px;
     color: ${({ theme }) => theme.colors.lightMode.textSecondary};
 `;
 
 export const Label = styled.span`
-    font-size: 18px;
-    color: ${({ theme }) => theme.colors.lightMode.textSecondary};
     margin: 8px 0;
+    color: ${({ theme }) => theme.colors.lightMode.textSecondary};
 `;
 
 export const Link = styled.a`
