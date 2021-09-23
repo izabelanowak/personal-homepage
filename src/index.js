b0d45c0f11709c6934b02964b31b826680f6bffa
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-09-23 16:47:23 +0200

    add normalize and provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Normalize } from "styled-normalize";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from './core/GlobalStyle';
 import App from './core/App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./core/theme";
+import store from './core/store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <Normalize />
         <GlobalStyle />
         <App />
       </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
