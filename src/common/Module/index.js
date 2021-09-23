cc68ae6f2c08876ef5ce1c216d5da9c02ecbb238
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-09-23 23:55:16 +0200

    add email link to footer

--- src/common/Module/index.js
+++ src/common/Module/index.js
@@ -3,10 +3,15 @@ import { Description, Extra, Text, Title, Wrapper } from "./styled";
 const Module = ({ text, title, description, extraContent, footer }) => (
   <Wrapper footer={footer}>
     <Text>{text}</Text>
-    <Title footer={footer}>{title}</Title>
+    <Title footer={footer}>
+      {footer
+        ? <a href={`mailto: ${title}`}>{title}</a>
+        : title
+      }
+    </Title>
     <Description footer={footer}>{description}</Description>
     <Extra>{extraContent}</Extra>
-  </Wrapper>
+  </Wrapper >
 );
 
 export default Module;
