9823129e1b2c23424d3c861d301d3d654b815d86
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-09-23 22:24:07 +0200

    add StatusChecker

---
+++ src/common/StatusChecker/index.js
@@ -0,0 +1,10 @@
+import {Loading} from "./../Loading";
+import {Error} from "./../Error";
+
+export const StatusChecker = ({ children, isLoading, isError }) => {
+  return (
+    <>
+    {isLoading ? <Loading /> : isError ? <Error /> : children}
+    </>
+  );
+};
--- src/features/homepage/Projects/index.js
+++ src/features/homepage/Projects/index.js
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Error } from "../../../common/Error";
+import { StatusChecker } from "../../../common/StatusChecker";
 import Teil from "../../../common/Teil";
-import { fetchRepos, selectRepos } from "../reposSlice";
+import { fetchRepos, selectError, selectLoading, selectRepos } from "../reposSlice";
+import { Wrapper } from "./styled";
 
 const Projects = () => {
   const demoBaseLink = "https://izabelanowak.github.io/";
+
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
   const repos = useSelector(selectRepos);
 
   useEffect(() => {
@@ -13,17 +19,19 @@ const Projects = () => {
   }, [dispatch]);
 
   return (
-    <>
-      {repos.map(project =>
-        <Teil
-          key={project.id}
-          title={project.name}
-          description={project.description}
-          demoLink={`${demoBaseLink}${project.name}`}
-          codeLink={project.html_url}
-        />
-      )}
-    </>
+    <StatusChecker isError={isError} isLoading={isLoading}>
+      <Wrapper>
+        {repos.map(project =>
+          <Teil
+            key={project.id}
+            title={project.name}
+            description={project.description}
+            demoLink={`${demoBaseLink}${project.name}`}
+            codeLink={project.html_url}
+          />
+        )}
+      </Wrapper>
+    </StatusChecker>
   );
 };
 export default Projects;
