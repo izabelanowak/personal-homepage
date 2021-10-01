51650d2101b630ad4f3c3385a8e8fb81b343831e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-10-01 21:43:54 +0200

    add colorNames, common and themeLigth and dark

---
+++ src/core/App/theme.js
@@ -0,0 +1,39 @@
+const colorNames = {
+  white: "#FFFFFF",
+  whiteLilac: "#FBFBFE",
+  slateGray: "#6E7E91",
+  mercury: "#E5E5E5",
+  mineShaft: "#252525",
+  dodgerBlue: "#2188FF",
+  scinceBlue: "#0366D6",
+};
+
+const common = {
+  breakpoints: {
+    mobile: 767,
+    tablet: 991,
+  }
+}
+
+export const themeLight = {
+  ...common,
+  colors: {
+    textPrimary: colorNames.mineShaft,
+    textSecondary: colorNames.slateGray,
+    white: colorNames.white,
+    mainAccent: colorNames.scinceBlue,
+    lightGray: colorNames.mercury,
+    background: colorNames.whiteLilac,
+  },
+};
+
+export const themeDark = {
+  ...common,
+  colors: {
+    textPrimary: colorNames.white,
+    textSecondary: colorNames.white,
+    white: colorNames.white,
+    mainAccent: colorNames.dodgerBlue,
+    background: colorNames.mineShaft,
+  },
+};
