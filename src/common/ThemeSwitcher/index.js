934bcf95ac055103289cf5f2ee3ff546df759f0e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-10-01 22:34:52 +0200

    rename ThemeSwitcher, state from redux

---
+++ src/common/ThemeSwitcher/index.js
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { selectIsDarkTheme, toogleTheme } from "../themeSlice";
+import darkModeButton from "./darkModeButton.svg";
+import lightModeButton from "./lightModeButton.svg";
+import { Button, Label, Wrapper } from "./styled";
+
+const ThemeSwitcher = () => {
+  const dispatch = useDispatch();
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
+  return (
+    <Wrapper>
+      <Label>
+        {isDarkTheme ? "Dark mode on" : "Dark mode off"}
+      </Label>
+      <Button onClick={() => dispatch(toogleTheme())}>
+        {<img src={isDarkTheme ? darkModeButton : lightModeButton} alt="theme switcher" />}
+      </Button>
+    </Wrapper>
+  );
+}
+
+export default ThemeSwitcher;
