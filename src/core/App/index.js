ff5559e965b50bff9c81e09b9ab601f5141c0dbc
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-10-01 21:44:17 +0200

    app.js renamed to index.js

---
+++ src/core/App/index.js
@@ -0,0 +1,31 @@
+import { skillset, whatToLearnNext } from "../../lists";
+import ModeSwitcher from "../../common/ModeSwitcher";
+import Portfolio from "../../common/Portfolio";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Footer from "../../common/Footer";
+import Projects from "../../features/personalHomepage/Projects";
+import { MainContainer } from "../../common/MainContainer";
+
+function App() {
+  return (
+    <MainContainer>
+      <ModeSwitcher />
+      <Header />
+      <Section
+        title="My skillset includes"
+        list={skillset}
+      />
+      <Section
+        title="What I want to learn next"
+        list={whatToLearnNext}
+      />
+      <Portfolio>
+        <Projects />
+      </Portfolio>
+      <Footer />
+    </MainContainer>
+  );
+};
+
+export default App;
