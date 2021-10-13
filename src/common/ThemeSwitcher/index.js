7c5f6608d6183fbca323f63bb70269b0a9040d8c
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-10-13 14:54:41 +0200

    change in import view

--- src/common/ThemeSwitcher/index.js
+++ src/common/ThemeSwitcher/index.js
@@ -1,5 +1,4 @@
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectIsDarkTheme, toogleTheme } from "../themeSlice";
 import { Box, Button, Icon, IconWrapper, Label, Wrapper } from "./styled";
 
